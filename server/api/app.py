40b34ada781ba5a9f0f01aa3f676cd72f4f86df4
Praneeth <praneethchakka23@gmail.com>
2025-11-24 21:23:28 +0530

    build: Delete `requirements.txt` and configure Flask app to listen on all network interfaces.

--- server/api/app.py
+++ server/api/app.py
@@ -194,7 +194,7 @@ if __name__ == '__main__':
     
     logger.info("✅ All environment variables set")
     logger.info("🚀 Starting LazarFlow Server...")
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
 
 
 
