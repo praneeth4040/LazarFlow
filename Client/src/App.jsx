bc668e487500249f71d17552165fb7b51e9974b4
Praneeth <praneethchakka23@gmail.com>
2025-12-03 14:35:16 +0530

    feat: Add Vercel Analytics to the client application.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
@@ -9,15 +10,18 @@ import './App.css'
 
 function App() {
   return (
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/live/:liveid" element={<LiveTournament />} />
-      </Routes>
-    </Router>
+    <>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/live/:liveid" element={<LiveTournament />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   )
 }
 
